fd270055b7be4ee71b8f6ec0c166b7f4b882fafa
zafercavdar <zafercavdar@yahoo.com>
2017-05-16 06:32:21 +0300

    customer added, shipment with bug

--- send_package_server.php
+++ send_package_server.php
@@ -1,38 +1,36 @@
 <?php
 	include("session.php");
 
-	mysqli_begin_transaction($db, MYSQLI_TRANS_START_READ_WRITE);
 	$cust_ID = $_POST['cust_ID'];
 	$receiver_name = $_POST['receiver_name'];
 	$receiver_phone = $_POST['receiver_phone'];
 	$to_st_ID = $_POST['to_st_ID'];
+	$package_ID = 10000;
 
-	if ($_POST['package1'] == "package1"){
-		echo "package 1 is selected <br>";
-		$wtype = $_POST['wtype'];
-		$ttype = $_POST['ttype'];
-		$content = $_POST['content'];
-		$weight = $_POST['weight'];
-	}
-
-	if ($_POST['package2'] == "package2"){
-		echo "package 2 is selected <br>";
-		$package_ID2 = $_POST['package_ID2'];
-		$wtype2 = $_POST['wtype2'];
-		$ttype2 = $_POST['ttype2'];
-		$content2 = $_POST['content2'];
-		$weight2 = $_POST['weight2'];
-	}
-
-	if ($_POST['package3'] == "package3"){
-		echo "package 3 is selected <br>";
-		$package_ID3 = $_POST['package_ID3'];
-		$wtype3 = $_POST['wtype3'];
-		$ttype3 = $_POST['ttype3'];
-		$content3 = $_POST['content3'];
-		$weight3 = $_POST['weight3'];
+	mysqli_begin_transaction($db, MYSQLI_TRANS_START_READ_WRITE);
+	$result = mysqli_query($db,"CALL assign_tn(@a);") or die("Query fail:" .mysqli_error($db));
+	if($result){
+		mysqli_commit($db);
+		echo "Call successfull <br>";
+	} else {
+		mysqli_rollback($db);
+		echo "Call failed <br>";
+		die();
 	}
 	
+	$result2 = mysqli_query($db,"select @a;") or die("Query fail:" .mysqli_error($db));
+	$row = mysqli_fetch_array($result2);
+	$tracknumber = $row[0];
+	echo $tracknumber;
+
+	$now = getdate();
+	$dateofshipment = $now[0];
+	$estimated = 0;
+	$cost = 0;
+	echo "<br>";
+	echo $dateofshipment;
+	echo "<br>";
+
 	$admin = $_SESSION['username'];
 	if($qsql = mysqli_query($db,"select st_ID from store where emp_ID in (SELECT emp_ID from employee where username = '$admin')")){
 		$row = mysqli_fetch_array($qsql);
@@ -40,30 +38,80 @@
 	} else {
 		echo "Error: This employee does not belong to any store!<br>";
 	}
-	
-	$now = getdate();
-	$dateofshipment = $now[0];
-	echo $dateofshipment;
-	echo "<br>";
 
-	$result = mysqli_query($db,"CALL assign_tn(@result)") or die("Query fail:" .mysqli_error($db));
-	$result = mysqli_query($db,"select @result") or die("Query fail:" .mysqli_error($db));
-	$row = mysqli_fetch_array($result);
-	echo $row[0];
+	$status = "on-the-way";
 
-	/*
-	$sql = "INSERT INTO package VALUES ('".$cust_ID."','".$package_ID."','".$wtype."','".$ttype."', '".$content."', '".floatval($weight)."')";
-	if ($result = mysqli_query($db,$sql)){
-		//echo "Package successfully added to the system!";
-		//echo "<br>";
-		header("Location: welcome_employee.php");
-		die();
+	mysqli_begin_transaction($db, MYSQLI_TRANS_START_READ_WRITE);
+	$master_query = mysqli_query($db,"INSERT INTO shipment VALUES ('".$tracknumber."','".$cust_ID."','".$receiver_name."','".$receiver_phone."', '".$from_st_ID."', '".$to_st_ID."','".$dateofshipment."','".$estimated."','".$status."','".$cost."')");
+
+	if ($master_query){
+		mysqli_commit($db);
+		echo "Shipment query is successfull!<br>";
+
 	} else {
-		//printf("Error: %s\n",mysqli_error($db));
-		//$message = "wrong answer";
-		//echo "<script type='text/javascript'>alert('$message');</script>";
-		header("Location: send_package.html");
+		printf("Error on shipment query: %s\n", mysqli_error($db));
+		mysqli_rollback($db);
 		die();
-	}*/
-	
+	}
+
+	if ($qsql and $result and $result2 and $master_query){
+		//mysqli_begin_transaction($db, MYSQLI_TRANS_START_READ_WRITE);
+		$doCommit = True;
+
+		if ($_POST['package1'] == "package1"){
+			echo "package 1 is selected <br>";
+			$wtype = $_POST['wtype1'];
+			$ttype = $_POST['ttype1'];
+			$content = $_POST['content1'];
+			$weight = $_POST['weight1'];
+			$sql = "INSERT INTO package VALUES ('".$cust_ID."','".$package_ID."','".$wtype."','".$ttype."', '".$content."', '".floatval($weight)."')";
+			$send1 = mysqli_query($db,$sql);
+			if (!$send1){
+				$doCommit = False;	
+				printf("Error on package 1: %s\n",mysqli_error($db));
+				echo "<br>";					
+			}
+		}
+
+		if ($_POST['package2'] == "package2"){
+			echo "package 2 is selected <br>";
+			$package_ID2 = $_POST['package_ID2'];
+			$wtype2 = $_POST['wtype2'];
+			$ttype2 = $_POST['ttype2'];
+			$content2 = $_POST['content2'];
+			$weight2 = $_POST['weight2'];
+			$sql = "INSERT INTO package VALUES ('".$cust_ID."','".$package_ID."','".$wtype2."','".$ttype2."', '".$content2."', '".floatval($weight2)."')";
+			$send2 = mysqli_query($db,$sql);
+			if (!$send2){
+				$doCommit = False;
+				printf("Error on package 2: %s\n",mysqli_error($db));
+				echo "<br>";											
+			}
+		}
+
+		if ($_POST['package3'] == "package3"){
+			echo "package 3 is selected <br>";
+			$package_ID3 = $_POST['package_ID3'];
+			$wtype3 = $_POST['wtype3'];
+			$ttype3 = $_POST['ttype3'];
+			$content3 = $_POST['content3'];
+			$weight3 = $_POST['weight3'];
+			$sql = "INSERT INTO package VALUES ('".$cust_ID."','".$package_ID."','".$wtype3."','".$ttype3."', '".$content3."', '".floatval($weight3)."')";
+			$send3 = mysqli_query($db,$sql);
+			if (!$send3){
+				$doCommit = False;
+				printf("Error on package 3: %s\n",mysqli_error($db));	
+				echo "<br>";							
+			}
+		}
+		
+		if($doCommit){
+			mysqli_commit($db);
+			echo "WE DID IT!";	
+		} else {
+			mysqli_rollback($db);
+			echo "fucked up!";
+		}
+	}
+
 ?>
